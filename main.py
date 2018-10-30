d2d032e4c6298a6fbdfa8bc876985e0bee03b787
Tyler Hadidon <hadidotj@gmail.com>
2018-10-30 06:21:24 +0100

    Writing simple main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from cmdserver import CmdServer
+import signal
+import sys
 
+server = None
+
+def shutdown(sig, frame):
+	if(server is not None):
+		server.stop()
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, shutdown)
+signal.signal(signal.SIGTERM, shutdown)
+
+server = CmdServer()
+
+signal.pause()
