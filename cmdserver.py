100fad083b4c9c16796070b76eede8acf313803f
Tyler Hadidon <hadidotj@gmail.com>
2018-11-24 17:43:52 +0000

    Reducing fin wait time back to 1 since it causes latency on the web UI when updating or setting temp.

--- cmdserver.py
+++ cmdserver.py
@@ -64,7 +64,7 @@ class CmdServer:
 			logger.warn('Client took too long to send a command')
 		except Exception as e:
 			logger.exception('Exception during client processing')
-		time.sleep(5)
+		time.sleep(1)
 		try:
 			client.shutdown(socket.SHUT_RDWR)
 			client.close()
